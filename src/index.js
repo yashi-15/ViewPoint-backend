878099741caa564d74073e86daac6f4d70773617
yashi-15 <sharmayashi270@gmail.com>
2025-12-05 20:28:12 +0530

    app ready, classes for api response and errors created, a async wrapper common function created

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
-dotenv.config()
+dotenv.config();
 
 connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () =>
+            console.log(`Server is running at port: ${process.env.PORT}`)
+        );
+    })
+    .catch((err) => console.log("Mongodb connection failed!!", err));
